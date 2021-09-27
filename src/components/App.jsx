77b71da81d9ce6295cce8f716736747964925601
neethunsr <neethu2001nsr@gmail.com>
2021-09-27 12:00:36 +0530

    Used arrow function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,19 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 
-function createCard(contact){
-  return <Card 
-    key={contact.id}
-    name={contact.name} 
-    img={contact.imgURL}
-    tel={contact.phone}
-    mail={contact.email}
-  />
-}
-
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      {contacts.map(createCard)}
+      {contacts.map(contact =>(
+        <Card 
+          key={contact.id}
+          name={contact.name} 
+          img={contact.imgURL}
+          tel={contact.phone}
+          mail={contact.email}
+        />
+      ))};
     </div>
   );
 }
